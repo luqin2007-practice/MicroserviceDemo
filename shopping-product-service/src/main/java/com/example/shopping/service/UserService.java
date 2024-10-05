75a89324f1b9cc76c49969b97287e05a4ba4e937
luqin2007 <lq2007lq@hotmail.com>
2024-10-05 17:58:51 +0800

    spring cloud

--- shopping-product-service/src/main/java/com/example/shopping/service/UserService.java
+++ shopping-product-service/src/main/java/com/example/shopping/service/UserService.java
@@ -2,6 +2,9 @@ package com.example.shopping.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value = "SHOPPING-USER-SERVICE", fallback = UserServiceFallback.class)
+@FeignClient(name = "SHOPPING-USER-SERVICE",
+//      url = "http://localhost:8285/shopping-user-service",
+        url = "${gateway.base-url}/shopping-user-service",
+        fallback = UserServiceFallback.class)
 public interface UserService extends com.example.shopping.api.UserService {
 }
