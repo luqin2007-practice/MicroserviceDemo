109d05cd3e7829e0a1790db67ef0b5e966c4e311
luqin2007 <lq2007lq@hotmail.com>
2024-10-10 02:13:30 +0800

    Micrometer Tracing

--- shopping-product-service/src/main/java/com/example/shopping/service/UserRemoteClient.java
+++ shopping-product-service/src/main/java/com/example/shopping/service/UserRemoteClient.java
@@ -1,12 +1,10 @@
 package com.example.shopping.service;
 
 import com.example.shopping.api.UserService;
-import io.micrometer.observation.annotation.Observed;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(name = "SHOPPING-USER-SERVICE",
         url = "${gateway.base-url}/userservice",
         fallbackFactory = UserFallbackFactory.class)
-@Observed
 public interface UserRemoteClient extends UserService {
 }
