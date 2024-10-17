85b2de21d7b8792d6a89cccc0241b1ca821dd9b9
luqin2007 <lq2007lq@hotmail.com>
2024-10-18 01:38:07 +0800

    oa2server

---
+++ shopping-oauth-server/src/test/java/com/example/shoppingoauthserver/ShoppingOauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingoauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingOauthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
